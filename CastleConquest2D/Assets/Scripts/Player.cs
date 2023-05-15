2da19e79a51b09cc7257683036a76c5d39179357
giovannilmaciel <giomaciel1@gmail.com>
2023-05-15 19:32:22 -0400

    Updated Run Script and Run Animation

--- CastleConquest2D/Assets/Scripts/Player.cs
+++ CastleConquest2D/Assets/Scripts/Player.cs
@@ -5,10 +5,15 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
+
+    Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +26,26 @@ public class Player : MonoBehaviour
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        print(controlThrow);
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
+
+        FlipSprite();
+        ChangingToRunningState();
+    }
+
+    private void ChangingToRunningState()
+    {
+        bool runningHorizontaly = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runningHorizontaly);
+    }
+
+    private void FlipSprite()
+    {
+        bool runningHorizontaly = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (runningHorizontaly)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+        }
     }
 }
