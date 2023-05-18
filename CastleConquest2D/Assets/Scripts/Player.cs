54b1bfb0ee414e9f67ab1707e036b4b1ab376fc8
giovannilmaciel <giomaciel1@gmail.com>
2023-05-18 03:37:48 -0400

    Added Decorations Sprites and Climbs mechanics

--- CastleConquest2D/Assets/Scripts/Player.cs
+++ CastleConquest2D/Assets/Scripts/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 15f;
+    [SerializeField] float climbSpeed = 8f;
 
     Rigidbody2D myRigidbody2D;
     Animator myAnimator;
     BoxCollider2D myBoxCollider2D;
     PolygonCollider2D myPlayersFeet;
+
+    float startingGravityScale;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,8 @@ public class Player : MonoBehaviour
         myAnimator = GetComponent<Animator>();
         myBoxCollider2D = GetComponent<BoxCollider2D>();
         myPlayersFeet = GetComponent<PolygonCollider2D>();
+
+        startingGravityScale = myRigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
@@ -28,6 +33,27 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        Climb();
+    }
+
+    private void Climb()
+    {
+        if (myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector2 climbVelocity = new Vector2(myRigidbody2D.velocity.x, controlThrow * climbSpeed);
+
+            myRigidbody2D.velocity = climbVelocity;
+
+            myAnimator.SetBool("Climbing", true);
+
+            myRigidbody2D.gravityScale = 0f;
+        }
+        else
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidbody2D.gravityScale = startingGravityScale;
+        }
     }
 
     private void Jump()
